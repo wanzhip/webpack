75608acdf4599da95c5fa15ebebbae88804c6e24
万志鹏 <1151333166@qq.com>
2020-11-13 18:24:22 +0800

    css loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
+import './index.css';
 function component() {
   var element = document.createElement('div');
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的  Shift + Alt + B
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   // element.innerHTML = ['Hello', 'webpack'].join(' ');
 
