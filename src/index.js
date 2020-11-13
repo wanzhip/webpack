947ecb973463791a24fed9a440a6000d70b8054e
万志鹏 <1151333166@qq.com>
2020-11-13 18:36:21 +0800

    图片处理

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
 import './index.css';
+import icon from './asserts/test.png';
 function component() {
   var element = document.createElement('div');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的  Shift + Alt + B
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   // element.innerHTML = ['Hello', 'webpack'].join(' ');
-
+  var myIcon = new Image();
+  myIcon.src = icon;
+  element.appendChild(myIcon);
   return element;
 }
 
